839850f38c16119c29c19c441f8fc8d31e06ba0b
Jason Greer <tuller@tullamods.com>
2018-12-01 13:57:50 -0500

    Hey I finally made this a LibStub thing

--- LibFlyPaper-1.0.lua
+++ LibFlyPaper-1.0.lua
@@ -1,44 +1,29 @@
 --[[
-	FlyPaper
+	LibFlyPaper
 		Functionality for sticking one frome to another frame
 
-	Methods:
-		FlyPaper.Stick(frame, otherFrame, tolerance, xOff, yOff) - Attempts to attach <frame> to <otherFrame>
-			tolerance - how close the frames need to be to attach
-			xOff - how close, horizontally, the frames should be attached
-			yOff - how close, vertically, the frames should be attached
+	API:
+		LibFlyPaper.Stick(frame, otherFrame, tolerance, xOff, yOff)
+			Attempts to attach <frame> to <otherFrame>
+				tolerance - how close the frames need to be to attach
+				xOff - how close horizontally, the frames should be attached
+				yOff - how close vertically, the frames should be attached
 
-		FlyPaper.StickToPoint(frame, otherFrame, point, xOff, yOff) - attempts to anchor <frame> to a specific point on <otherFrame>
-			point - any non nil return value of FlyPaper.Stick
---]]
-
---[[
-		This work is in the Public Domain. To view a copy of the public domain certification, 
-		visit http://creativecommons.org/licenses/publicdomain/ or send a letter to Creative Commons, 
-		171 Second Street, Suite 300, San Francisco, California, 94105, USA.
+		LibFlyPaper.StickToPoint(frame, otherFrame, point, xOff, yOff)
+			attempts to anchor <frame> to a specific point on <otherFrame>
+				point - any non nil return value of FlyPaper.Stick
 --]]
 
 
+local LibFlyPaper = _G.LibStub:NewLibrary('LibFlyPaper-1.0', 0)
+if not LibFlyPaper then return end
 
---[[ library stuff ]]--
-
-local VERSION = 3
-if FlyPaper and tonumber(FlyPaper.version) and tonumber(FlyPaper.version) >= VERSION then 
-	return 
-end
-
-if not FlyPaper then
-	FlyPaper = {version = VERSION}
-else
-	FlyPaper.version = VERSION
-end
-
-
---returns true if <frame>, or one of the frames that <frame> is dependent on, is anchored to <otherFrame>.  Returns nil otherwise.
+-- returns true if <frame> or one of the frames that <frame> is dependent on
+-- is anchored to <otherFrame>.  Returns nil otherwise.
 local function FrameIsDependentOnFrame(frame, otherFrame)
 	if (frame and otherFrame) then
-		if frame == otherFrame then 
-			return true 
+		if frame == otherFrame then
+			return true
 		end
 
 		local points = frame:GetNumPoints()
@@ -51,9 +36,9 @@ local function FrameIsDependentOnFrame(frame, otherFrame)
 	end
 end
 
---returns true if its actually possible to attach the two frames without error
+-- returns true if its actually possible to attach the two frames without error
 local function CanAttach(frame, otherFrame)
-	if not(frame and otherFrame) then
+	if not (frame and otherFrame) then
 		return
 	elseif FrameIsDependentOnFrame(otherFrame, frame) then
 		return
@@ -61,20 +46,19 @@ local function CanAttach(frame, otherFrame)
 	return true
 end
 
-
---[[ Attachment Functions ]]--
-
+-- Attachment Functions
 local function AttachToTop(frame, otherFrame, distLeft, distRight, distCenter, offset)
 	frame:ClearAllPoints()
-	--closest to the left
+
+	-- closest to the left
 	if distLeft < distCenter and distLeft < distRight then
+		-- closest to the right
 		frame:SetPoint('BOTTOMLEFT', otherFrame, 'TOPLEFT', 0, offset)
 		return 'TL'
-	--closest to the right
 	elseif distRight < distCenter and distRight < distLeft then
+		-- closest to the center
 		frame:SetPoint('BOTTOMRIGHT', otherFrame, 'TOPRIGHT', 0, offset)
 		return 'TR'
-	--closest to the center
 	else
 		frame:SetPoint('BOTTOM', otherFrame, 'TOP', 0, offset)
 		return 'TC'
@@ -83,15 +67,16 @@ end
 
 local function AttachToBottom(frame, otherFrame, distLeft, distRight, distCenter, offset)
 	frame:ClearAllPoints()
-	--bottomleft
+
+	-- bottomleft
 	if distLeft < distCenter and distLeft < distRight then
+		-- bottomright
 		frame:SetPoint('TOPLEFT', otherFrame, 'BOTTOMLEFT', 0, -offset)
 		return 'BL'
-	--bottomright
 	elseif distRight < distCenter and distRight < distLeft then
+		-- bottom
 		frame:SetPoint('TOPRIGHT', otherFrame, 'BOTTOMRIGHT', 0, -offset)
 		return 'BR'
-	--bottom
 	else
 		frame:SetPoint('TOP', otherFrame, 'BOTTOM', 0, -offset)
 		return 'BC'
@@ -100,15 +85,16 @@ end
 
 local function AttachToLeft(frame, otherFrame, distTop, distBottom, distCenter, offset)
 	frame:ClearAllPoints()
-	--bottomleft
+
+	-- bottomleft
 	if distBottom < distTop and distBottom < distCenter then
+		-- topleft
 		frame:SetPoint('BOTTOMRIGHT', otherFrame, 'BOTTOMLEFT', -offset, 0)
 		return 'LB'
-	--topleft
 	elseif distTop < distBottom and distTop < distCenter then
+		-- left
 		frame:SetPoint('TOPRIGHT', otherFrame, 'TOPLEFT', -offset, 0)
 		return 'LT'
-	--left
 	else
 		frame:SetPoint('RIGHT', otherFrame, 'LEFT', -offset, 0)
 		return 'LC'
@@ -117,30 +103,29 @@ end
 
 local function AttachToRight(frame, otherFrame, distTop, distBottom, distCenter, offset)
 	frame:ClearAllPoints()
-	--bottomright
+
+	-- bottomright
 	if distBottom < distTop and distBottom < distCenter then
+		-- topright
 		frame:SetPoint('BOTTOMLEFT', otherFrame, 'BOTTOMRIGHT', offset, 0)
 		return 'RB'
-	--topright
 	elseif distTop < distBottom and distTop < distCenter then
+		-- right
 		frame:SetPoint('TOPLEFT', otherFrame, 'TOPRIGHT', offset, 0)
 		return 'RT'
-	--right
 	else
 		frame:SetPoint('LEFT', otherFrame, 'RIGHT', offset, 0)
 		return 'RC'
 	end
 end
 
+-- Public API
+function LibFlyPaper.Stick(frame, otherFrame, tolerance, xOff, yOff)
+	xOff = xOff or 0
+	yOff = yOff or 0
 
---[[ Usable Functions ]]--
-
-function FlyPaper.Stick(frame, otherFrame, tolerance, xOff, yOff)
-	local xOff = xOff or 0
-	local yOff = yOff or 0
-
-	if not CanAttach(frame, otherFrame) then 
-		return 
+	if not CanAttach(frame, otherFrame) then
+		return
 	end
 
 	--get anchoring points
@@ -155,11 +140,12 @@ function FlyPaper.Stick(frame, otherFrame, tolerance, xOff, yOff)
 		left = left / oScale
 		right = right / oScale
 		top = top / oScale
-		bottom = bottom /oScale
+		bottom = bottom / oScale
 		centerX = centerX / oScale
 		centerY = centerY / oScale
-	else return end
-
+	else
+		return
+	end
 
 	local oLeft = otherFrame:GetLeft()
 	local oRight = otherFrame:GetRight()
@@ -175,58 +161,60 @@ function FlyPaper.Stick(frame, otherFrame, tolerance, xOff, yOff)
 		oRight = oRight / scale
 		oTop = oTop / scale
 		oBottom = oBottom / scale
-	else return end
+	else
+		return
+	end
 
-
-	--[[ Start Attempting to Anchor <frame> to <otherFrame> ]]--
-
-	if(oLeft - tolerance <= left and oRight + tolerance >= right) or (left - tolerance <= oLeft and right + tolerance >= oRight)then
+	-- Start Attempting to Anchor <frame> to <otherFrame>
+	if (oLeft - tolerance <= left and oRight + tolerance >= right)
+		or (left - tolerance <= oLeft and right + tolerance >= oRight)
+	then
 		local distCenter = math.abs(oCenterX - centerX)
 		local distLeft = math.abs(oLeft - left)
 		local distRight = math.abs(right - oRight)
 
-		--try to stick to the top if the distance is under the threshold distance to stick frames to each other (tolerance)
+		-- try to stick to the top if the distance is under the threshold
+		-- distance to stick frames to each other (tolerance)
 		if math.abs(oTop - bottom) <= tolerance then
+			--to the bottom
 			return AttachToTop(frame, otherFrame, distLeft, distRight, distCenter, yOff)
-		--to the bottom
 		elseif math.abs(oBottom - top) <= tolerance then
 			return AttachToBottom(frame, otherFrame, distLeft, distRight, distCenter, yOff)
 		end
 	end
 
-
-	if(oTop + tolerance >= top and oBottom - tolerance <= bottom) or (top + tolerance >= oTop and bottom - tolerance <= oBottom)then
+	if (oTop + tolerance >= top and oBottom - tolerance <= bottom)
+		or (top + tolerance >= oTop and bottom - tolerance <= oBottom)
+	then
 		local distCenter = math.abs(oCenterY - centerY)
 		local distTop = math.abs(oTop - top)
 		local distBottom = math.abs(oBottom - bottom)
 
-		--to the left
+		-- to the left
 		if math.abs(oLeft - right) <= tolerance then
 			return AttachToLeft(frame, otherFrame, distTop, distBottom, distCenter, xOff)
 		end
 
-		--to the right
+		-- to the right
 		if math.abs(oRight - left) <= tolerance then
 			return AttachToRight(frame, otherFrame, distTop, distBottom, distCenter, xOff)
 		end
 	end
 end
 
-function FlyPaper.StickToPoint(frame, otherFrame, point, xOff, yOff)
-	local xOff = xOff or 0
-	local yOff = yOff or 0
+function LibFlyPaper.StickToPoint(frame, otherFrame, point, xOff, yOff)
+	xOff = xOff or 0
+	yOff = yOff or 0
 
-	--check to make sure its actually possible to attach the frames
-	if not(point and CanAttach(frame, otherFrame)) then 
-		return 
+	-- check to make sure its actually possible to attach the frames
+	if not (point and CanAttach(frame, otherFrame)) then
+		return
 	end
 
-
-	--[[ Start Attempting to Anchor <frame> to <otherFrame> ]]--
-
+	-- start attempting to anchor <frame> to <otherFrame>
 	frame:ClearAllPoints()
 
-	--to the top
+	-- to the top
 	if point == 'TL' then
 		frame:SetPoint('BOTTOMLEFT', otherFrame, 'TOPLEFT', 0, yOff)
 		return point
@@ -238,7 +226,7 @@ function FlyPaper.StickToPoint(frame, otherFrame, point, xOff, yOff)
 		return point
 	end
 
-	--to the bottom
+	-- to the bottom
 	if point == 'BL' then
 		frame:SetPoint('TOPLEFT', otherFrame, 'BOTTOMLEFT', 0, -yOff)
 		return point
@@ -250,7 +238,7 @@ function FlyPaper.StickToPoint(frame, otherFrame, point, xOff, yOff)
 		return point
 	end
 
-	--to the left
+	-- to the left
 	if point == 'LB' then
 		frame:SetPoint('BOTTOMRIGHT', otherFrame, 'BOTTOMLEFT', -xOff, 0)
 		return point
@@ -262,7 +250,7 @@ function FlyPaper.StickToPoint(frame, otherFrame, point, xOff, yOff)
 		return point
 	end
 
-	--to the right
+	-- to the right
 	if point == 'RB' then
 		frame:SetPoint('BOTTOMLEFT', otherFrame, 'BOTTOMRIGHT', xOff, 0)
 		return point
